f93855699c7dc3bc80fde90fbe767b576e5471e5
wikty <xiaowenbin_999@163.com>
2018-11-21 13:45:09 +0800

    add gird search

--- hyperparams.py
+++ hyperparams.py
@@ -7,83 +7,36 @@ import subprocess
 
 import config
 from lib.utils import Params
+from lib.hyperparam_optim.grid_search import Searcher
 
 
-def train_model(model_dir, data_dir, checkpoint):
+def train_model(exp_dir, data_dir, restore_checkpoint):
     args = [sys.executable, 
             'train.py',
-            '--model-dir', model_dir,
+            '--exp-dir', exp_dir,
             '--data-dir', data_dir]
-    if checkpoint is not None:
-        args.extend(['--checkpoint', checkpoint])
+    if restore_checkpoint is not None:
+        args.extend(['--restore-checkpoint', restore_checkpoint])
     subprocess.run(args, check=True, shell=True)
-
-
-def search_all(hyperparams, parent_dir, data_dir, checkpoint,
-               params_filename):
-    assert isinstance(hyperparams, dict)
-    params_file = os.path.join(parent_dir, params_filename)
-    msg = "Parent directory doesn't contain params file."
-    assert os.path.isfile(params_file), msg
-
-    # create experiment directory and copy params file
-    hyperparams_dir = os.path.join(parent_dir, 'hyperparams')
-    if os.path.isdir(hyperparams_dir):
-        shutil.rmtree(hyperparams_dir)
-    os.makedirs(hyperparams_dir)
-    shutil.copy(params_file, 
-                os.path.join(hyperparams_dir, params_filename))
-
-    pds = [hyperparams_dir]
-    for name, candidates in hyperparams.items():
-        new_pds = []
-        for pd in pds:
-            model_dirs = search_hyperparam((name, candidates), 
-                pd, data_dir, checkpoint, params_filename)
-            new_pds.extend(model_dirs)
-        pds = new_pds
-    return hyperparams_dir
-
-
-def search_hyperparam(hyperparam, parent_dir, data_dir, checkpoint,
-                      params_filename):
-    assert isinstance(hyperparam, tuple)
-    params_file = os.path.join(parent_dir, params_filename)
-    msg = "Parent directory doesn't contain params file."
-    assert os.path.isfile(params_file), msg
-
-    params = Params(params_file)
-    name, candidates = hyperparam
-    model_dirs = []
-    for candidate in candidates:
-        experiment = '{}_{}'.format(name, candidate)
-        model_dir = os.path.join(parent_dir, experiment)
-        if not os.path.isdir(model_dir):
-            os.makedirs(model_dir)
-        model_dirs.append(model_dir)
-        # create params file for the experiment
-        params.set(name, candidate)
-        params.dump(os.path.join(model_dir, params_filename))
-        # run subprocess to train model
-        train_model(model_dir, data_dir, checkpoint)
-    return model_dirs
 
 
 if __name__ == '__main__':
     data_dir = config.data_dir
-    model_dir = config.base_model_dir
-    params_filename = config.params_file
+    exp_dir = config.base_model_dir
+
+
+    params_filename = 'params.json'
 
     # define command line parser
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-dir', 
                         default=data_dir, 
                         help="The directory containing the datasets")
-    parser.add_argument('--model-dir', 
-                        default=model_dir, 
+    parser.add_argument('--exp-dir', 
+                        default=exp_dir, 
                         help="The directory contains hyperparameters \
                         config file and will store log and result files.")
-    parser.add_argument('--checkpoint', 
+    parser.add_argument('--restore-checkpoint', 
                         default=None,
                         help="The name of checkpoint to restore model")
     parser.add_argument('--job',
@@ -93,15 +46,15 @@ if __name__ == '__main__':
     # parse command line arguments
     args = parser.parse_args()
     data_dir = args.data_dir
-    model_dir = args.model_dir
-    checkpoint = args.checkpoint
+    exp_dir = args.exp_dir
+    restore_checkpoint = args.restore_checkpoint
     job = args.job
     msg = "Data directory not exists: {}"
     assert os.path.isdir(data_dir), msg.format(data_dir)
-    msg = "Model directory not exists: {}"
-    assert os.path.isdir(model_dir), msg.format(model_dir)
-    params_file = os.path.join(model_dir, params_filename)
-    msg = "Model config file not exists: {}"
+    msg = "Experiment directory not exists: {}"
+    assert os.path.isdir(exp_dir), msg.format(exp_dir)
+    params_file = os.path.join(exp_dir, params_filename)
+    msg = "Experiment config file not exists: {}"
     assert os.path.isfile(params_file), msg.format(params_file)
 
     # config your experiment learning rates
@@ -110,18 +63,30 @@ if __name__ == '__main__':
         'batch_size': [10, 50, 100],
         'embedding_dim': [50, 100, 200]
     }
-    if job == 'lr':
-        search_hyperparam(('learning_rate', hyperparams['learning_rate']),
-                          model_dir, data_dir, checkpoint, params_filename)
-    elif job == 'bz':
-        search_hyperparam(('batch_size', hyperparams['batch_size']),
-                          model_dir, data_dir, checkpoint, params_filename)
-    elif job == 'ed':
-        search_hyperparam(('embedding_dim', hyperparams['embedding_dim']),
-                          model_dir, data_dir, checkpoint, params_filename)
-    elif job == 'all':
-        search_all(hyperparams, model_dir, data_dir, checkpoint, 
-                   params_filename)
-    else:
-        print('Please specify the valid job name!')
+
+    searcher = Searcher(exp_dir, params_filename, train_model, {
+        'data_dir': data_dir,
+        'exp_dir': exp_dir,
+        'restore_checkpoint': restore_checkpoint
+    })
+
+    searcher.run({
+        'learning_rate': [1e-4, 1e-3, 1e-2],
+
+    })
+
+    # if job == 'lr':
+    #     search_hyperparam(('learning_rate', hyperparams['learning_rate']),
+    #                       model_dir, data_dir, checkpoint, params_filename)
+    # elif job == 'bz':
+    #     search_hyperparam(('batch_size', hyperparams['batch_size']),
+    #                       model_dir, data_dir, checkpoint, params_filename)
+    # elif job == 'ed':
+    #     search_hyperparam(('embedding_dim', hyperparams['embedding_dim']),
+    #                       model_dir, data_dir, checkpoint, params_filename)
+    # elif job == 'all':
+    #     search_all(hyperparams, model_dir, data_dir, checkpoint, 
+    #                params_filename)
+    # else:
+    #     print('Please specify the valid job name!')
     
---
+++ lib/hyperparam_optim/grid_search.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+
+from lib.utils import Params
+
+
+class Searcher(object):
+
+    def __init__(self, experiment_dir, params_filename, learner, 
+                 learner_kwargs):
+        assert os.path.isdir(experiment_dir)
+        assert os.path.isfile(os.path.join(experiment_dir, params_filename))
+        assert hasattr(learner, '__call__')
+        assert isinstance(learner_kwargs, dict)
+        self.experiment_dir = experiment_dir
+        self.experiment_params = Params(os.path.join(experiment_dir, 
+                                                     params_filename))
+        self.params_filename = params_filename
+        self.learner = learner
+        self.learner_kwargs = learner_kwargs
+        self.grid_search_dirname = 'search_gird'
+
+    def search(self, hyperparam, parent_dir):
+        assert isinstance(hyperparam, tuple)
+        name, candidates = hyperparam
+        params = Params(os.path.join(parent_dir, self.params_filename))
+        experiment_dirs = []
+        for candidate in candidates:
+            experiment = '{}_{}'.format(name, candidate)
+            experiment_dir = os.path.join(parent_dir, experiment)
+            # create experiment directory
+            if not os.path.isdir(experiment_dir):
+                os.makedirs(experiment_dir)
+            experiment_dirs.append(experiment_dir)
+            # create params file for this experiment
+            params.set(name, candidate)
+            params.dump(os.path.join(experiment_dir, self.params_filename))
+            # run subprocess to train model
+            self.learner(**self.learner_kwargs)
+        return experiment_dirs
+
+    def run(self, hyperparams):
+        assert isinstance(hyperparams, dict)
+        # create grid search directory
+        parent_dir = os.path.join(self.experiment_dir, 
+                                  self.grid_search_dirname)
+        if os.path.isdir(parent_dir):
+            shutil.rmtree(parent_dir)
+        os.makedirs(parent_dir)
+        # create experiment params file
+        self.experiment_params.dump(os.path.join(parent_dir, 
+                                                 self.params_filename))
+        # gird search
+        pds = [parent_dir]
+        for name, candidates in hyperparams.items():
+            new_pds = []
+            for pd in pds:
+                experiment_dirs = self.search((name, candidates), pd)
+                new_pds.extend(experiment_dirs)
+            pds = new_pds
+        return parent_dir
