a6b7ff9da6939ce622085829266cf4c9c9aeab53
wikty <xiaowenbin_999@163.com>
2018-11-21 09:11:05 +0800

    record latest metric

--- lib/utils/best_metric_recorder.py
+++ lib/utils/best_metric_recorder.py
@@ -2,7 +2,10 @@ class BestMetricRecorder(object):
 
     def __init__(self, init_value, metric_name):
         self.best_value = init_value
+        self.latest_value = init_value
         self.metric_name = metric_name
+        self.best_metric = None
+        self.latest_metric = None
 
     def __call__(self):
         return self.best_value
@@ -10,11 +13,17 @@ class BestMetricRecorder(object):
     def value(self):
         return self.best_value
 
+    def metric(self):
+        return self.best_metric
+
     def improved(self, results):
         assert (self.metric_name in results)
         if self.compare(results[self.metric_name], self.best_value) > 0:
             self.best_value = results[self.metric_name]
+            self.best_metric = results
             return True
+        self.latest_value = results[self.metric_name]
+        self.latest_metric = results
         return False
 
     def compare(self, current_value, best_value):
